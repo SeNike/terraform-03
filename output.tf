6c956b617542be421a4bf46540595837ff6f4ed7
SeNike <nsvtemp@gmail.com>
2024-09-05 13:02:57 +0000

    task 5

--- output.tf
+++ output.tf
@@ -1,3 +1,14 @@
-output "storage_attributes" {
-  value = yandex_compute_instance.storage
+output "vm_list" {
+  value = flatten([
+    for all in [
+      yandex_compute_instance.web,
+      yandex_compute_instance.db
+    ] : [
+      for i in all : {
+        name = i.name
+        id   = i.id
+        fqdn = i.fqdn
+      }
+    ]
+  ])
 }
